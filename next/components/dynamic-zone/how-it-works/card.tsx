688fcbd644fc3cfdd9aadfeb2a2fff65b8e47d17
Ong Dun Yan <ongdunyan@Ongs-MacBook-Air.local>
2025-08-11 00:02:53 +0800

    update Card to also accept index as a prop

--- next/components/dynamic-zone/how-it-works/card.tsx
+++ next/components/dynamic-zone/how-it-works/card.tsx
@@ -5,13 +5,17 @@ import React from "react";
 export const Card = ({
   title,
   description,
+  index,
 }: {
   title: string;
   description: string;
+  index: number;
 }) => {
   return (
     <div className="max-w-xl mx-auto my-6 p-6 rounded-md border border-neutral-800 bg-neutral-950">
-      <p className="text-lg font-bold text-white mb-2">{title}</p>
+      <p className="text-lg font-bold text-white mb-2">
+        {index}. {title}
+      </p>
       <p className="text-neutral-400">{description}</p>
     </div>
   );
